18437cb1f2deab43951c63b32b8c7c1134c345a2
Nick Leon <nicolas7@vt.edu>
2022-02-27 15:09:20 -0500

    test front end files

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+//------------------------------------------------------new
+const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+const db = require('./config/connection');
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+server.applyMiddleware({ app });
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Server is up!')
-})
+const publicPath = path.join(__dirname, './client/', 'build/');
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
+});
+//------------------------------------------------------new
+// NOTE THIS FUNCTION ABSORBED ABOVE
+// app.get('/', (req, res) => {
+//   res.send('Server is up!')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
