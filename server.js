083c91e21efa68f0c9bbe9deac6906e1a9d74168
Nick Leon <nicolas7@vt.edu>
2022-02-27 21:19:54 -0500

    remove db and apollo server

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
-const db = require('./config/connection');
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
+// const db = require('./config/connection');
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: authMiddleware,
+// });
 // server.applyMiddleware({ app });
 // app.use(express.urlencoded({ extended: false }));
 // app.use(express.json());
