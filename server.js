c6fa57fd0c6bec450168e359d9716930dae4911e
Nick Leon <nicolas7@vt.edu>
2022-02-27 21:23:03 -0500

    require db

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
-// const db = require('./config/connection');
+const db = require('./config/connection');
 // const server = new ApolloServer({
 //   typeDefs,
 //   resolvers,
