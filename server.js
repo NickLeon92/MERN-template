68749301baaac6c27a95d1d39f65c8c61b4b2a61
Nick Leon <nicolas7@vt.edu>
2022-02-27 20:39:59 -0500

    change res.send

--- server.js
+++ server.js
@@ -18,14 +18,14 @@ app.use(express.json());
 
 const publicPath = path.join(__dirname, './client/', 'build/');
 app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
-});
+// app.get('*', (req, res) => {
+//    res.sendFile(path.join(publicPath, 'index.html'));
+// });
 //------------------------------------------------------new
 // NOTE THIS FUNCTION ABSORBED ABOVE
-// app.get('/', (req, res) => {
-//   res.send('Server is up!')
-// })
+app.get('/', (req, res) => {
+  res.send('Server is up!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
