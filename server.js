a275e9231e0d355260b9838196ddc98170e9369f
Nick Leon <nicolas7@vt.edu>
2022-02-28 14:15:15 -0500

    exclude urlencode and express.json

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 server.applyMiddleware({ app });
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
 // const publicPath = path.join(__dirname, './client/', 'build/');
 // app.use(express.static(publicPath));
